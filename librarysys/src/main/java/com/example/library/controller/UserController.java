2f39c6ca6065f484637492c56ec9dba5b0efc35d
Dhanshree Kurade <dhanshreekurade21@gmail.com>
2024-05-20 11:50:10 +0530

    initial commit

---
+++ librarysys/src/main/java/com/example/library/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.library.controller;
+
+import com.example.library.entity.User;
+import com.example.library.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.findAll();
+    }
+    @PostMapping
+    public User addUser(@RequestBody User user){
+        return userService.save(user);
+    }
+}
